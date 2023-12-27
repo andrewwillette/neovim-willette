bbe76bbcd1e6cf356ea93d7da2fd2217a890380a
andrewwillette <willette.andrew1846@gmail.com>
2023-12-27 13:51:38 -0600

    issue26452: resolving build issues

--- src/nvim/map_defs.h
+++ src/nvim/map_defs.h
@@ -163,11 +163,11 @@ KEY_DECLS(HlEntry)
 KEY_DECLS(ColorKey)
 KEY_DECLS(SignRange)
 
-/* MAP_DECLS(int, int) */
 /* MAP_DECLS(cstr_t, int) */
 /* MAP_DECLS(uint32_t, ptr_t) */
 /* MAP_DECLS(uint64_t, ssize_t) */
 /* MAP_DECLS(uint64_t, uint64_t) */
+MAP_DECLS(int, int)
 MAP_DECLS(ptr_t, ptr_t)
 MAP_DECLS(int64_t, ptr_t)
 MAP_DECLS(uint32_t, uint32_t)
